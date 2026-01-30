a800a63183f8f41ef7b320bfbefd91ee2023e2cc
ph07130713 <ph07130713@gmail.com>
2026-01-30 10:27:37 +0000

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,6 +15,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-KCCKHQW9VE"
+        />
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KCCKHQW9VE');`}
+        </Script>
+      </head>
       <body className="antialiased">{children}</body>
     </html>
   );
